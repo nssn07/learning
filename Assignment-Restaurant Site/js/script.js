7d2b9a85c69a353814396b62883314689ecdcc3b
nssn07 <97320513+nssn07@users.noreply.github.com>
2022-02-03 17:53:47 +0530

    Update script.js

--- Assignment-Restaurant Site/js/script.js
+++ Assignment-Restaurant Site/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-       alert(randomhtml);
+      // alert(randomhtml);
        var homeHtmlToInsertIntoMainPage =  insertHtml("#main-content", randomhtml);
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
